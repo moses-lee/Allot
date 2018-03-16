bc4dc215686b2c45a16ade5b05ff73c9ed7d4413
spotzdevelopment <spotzdevelopment@gmail.com>
2018-03-16 03:28:29 -0700

    First Commit

---
+++ app/src/main/java/com/wordpress/necessitateapps/allot/Getter.java
@@ -0,0 +1,28 @@
+package com.wordpress.necessitateapps.allot;
+
+/**
+ * Created by spotzdevelopment on 3/9/2018.
+ */
+
+public class Getter {
+    public Getter(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String name, username;
+
+}
