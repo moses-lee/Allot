8f28d070ecf8405aebb1ea177d8c121959e7472c
spotzdevelopment <spotzdevelopment@gmail.com>
2018-04-05 21:55:48 -0700

    Added first fragment

--- app/src/main/java/com/wordpress/necessitateapps/allot/MainActivity.java
+++ app/src/main/java/com/wordpress/necessitateapps/allot/MainActivity.java
@@ -1,18 +1,26 @@
 package com.wordpress.necessitateapps.allot;
 
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity{
 
-
+    @BindView(R.id.frame)
+    FrameLayout frameLayout;
+    FragmentManager fragmentManager = getSupportFragmentManager();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        //sets ExploreFragment when started
+        fragmentManager.beginTransaction().add(R.id.frame, new CalendarFragment()).commit();
     }
 }
